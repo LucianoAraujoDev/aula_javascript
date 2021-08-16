6dee7b4b8d622b643ea21a2109046ce8a5d4deee
LucianoAraujoDev <luciano.datatraining@gmail.com>
2021-08-16 15:51:50 -0300

    biblioteca e arrays com js

--- js/main.js
+++ js/main.js
@@ -3,10 +3,20 @@ var nome = "Luciano Araujo";
 var idade = 40;
 var idade2 = 19;
 var frase = "Japão é o melhor time do mundo";
+//var lista = ["maça","laranja","uva"];
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+
+//lista.push("banana");
+//lista.pop();
+//console.log(lista);
+console.log(frutas);
+alert(frutas[1].nome);
+//console.log(lista.length);
 
 //alert (nome + " tem " + idade + " anos");
 //alert (idade + idade2);
-console.log(nome);
-console.log(idade + idade2); 
+//console.log(nome);
+//console.log(idade + idade2); 
 //console.log(frase.replace("Japão","Brasil"));
-console.log(frase.toUpperCase());
+//console.log(frase.toUpperCase());
+
