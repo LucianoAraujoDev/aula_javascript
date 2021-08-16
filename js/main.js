20cb5612c963861ac57347423102a5b5944c4fd1
LucianoAraujoDev <luciano.datatraining@gmail.com>
2021-08-16 15:11:53 -0300

    trabalhando com strings no javascript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var nome = "Luciano Araujo";
+var idade = 40;
+var idade2 = 19;
+var frase = "Japão é o melhor time do mundo";
+
+//alert (nome + " tem " + idade + " anos");
+//alert (idade + idade2);
+console.log(nome);
+console.log(idade + idade2); 
+//console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
