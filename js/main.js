eaade4e7059edf700bad7badc5953c3cadf19a0b
LucianoAraujoDev <luciano.datatraining@gmail.com>
2021-08-19 17:56:50 -0300

    resolvendo exercicios do bootcamp de javascript

--- js/main.js
+++ js/main.js
@@ -43,4 +43,65 @@ if (idade >= 18){
 //lista.push("polonia");
 //lista.pop("Inglaterra");
 
-var lista = ["maça","laranja","uva"]
+/*var total = 0;
+
+var i = 0;
+
+var valor;
+
+while (i < 6)
+
+{
+
+ valor = 8;
+
+ if (valor > 0)
+
+ {
+
+   total = total + 1;
+
+ }
+
+ i = i + 1;
+
+}
+
+console.log(total + " valores");
+*/
+
+/*var N = 8;
+var inicio =  1;
+
+
+while (inicio <= N) {
+
+ if (inicio % 2 == 0) {
+
+   console.log(inicio);
+
+  } 
+  inicio++;
+
+}
+*/
+var valor = 576;
+var resto = 0;
+
+nota100 = Math.floor(valor / 100);
+resto = valor % 100;
+nota50  = Math.floor(resto/50);
+resto = resto % 50;
+nota20  = Math.floor(resto/20);
+resto = resto % 20;
+nota10  = Math.floor(resto/10);
+resto = resto % 10;
+nota5  = Math.floor(resto/5);
+resto = resto % 5;
+nota2  = Math.floor(resto/2);
+resto = resto % 2;
+nota1  = Math.floor(resto/1);
+
+console.log(nota100);
+console.log(nota50);
+console.log(nota20);
